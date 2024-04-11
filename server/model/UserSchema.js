8894c37f3e48bcd26cfc345de1a1a022ec430629
Jonathan Nazareth <jonathannazareth@hotmail.co.uk>
2024-04-11 17:07:35 +0100

    initial

---
+++ server/model/UserSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    bio: {
+      type: String,
+      required: [true, "Please add a bio"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("USER", userSchema);
+
+module.exports = User;
